ca2fe8eb4e06dc92521f655f0d3acc2f8857a0bf
kistakki <takacspetra95@gmail.com>
2023-11-08 12:44:27 +0100

    s

--- script.js
+++ script.js
@@ -8,7 +8,30 @@ const characterComponent = (characterData) => `
 <h2>${characterData.name}</h2>
 <h3>appears in: ${characterData.episode.length} episodes </h3>
 </div>
-`;
+`
+
+const buttonComponent = (id, text) => `<button id=${id}>${text}</button>`
+
+const makeDomFromDta = (data,rootElement) => {
+    const info = data.info
+    const characters = data.results
+
+    characters.forEach(character =>
+        rootElement.insertAdjacentHTML(
+          "beforeend",
+          characterComponent(character)))
+
+console.log(info)
+
+if(info.prev){
+    rootElement.insertAdjacentHTML("beforeend", buttonComponent("prev", "previous"))
+}
+if(info.next) {
+    rootElement.insertAdjacentHTML("beforeend", buttonComponent("next","next"))
+}
+}
+
+
 
 const init = () => {
   fetchUrl("https://rickandmortyapi.com/api/character").then((data) => {
