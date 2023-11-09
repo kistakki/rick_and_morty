b99861e89541f28fbbc2023eac008ef2a1ee18e6
kistakki <takacspetra95@gmail.com>
2023-11-09 17:37:06 +0100

    done

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
-const rootElement = document.querySelector("#root");
+const rootElement = document.querySelector("#root")
 
-const fetchUrl = (url) => fetch(url).then((res) => res.json());
+const fetchUrl = (url) => fetch(url).then(res => res.json())
 
-const characterComponent = (characterData) => `
-<div class ="char">
-<img src=${characterData.image}>
-<h2>${characterData.name}</h2>
-<h3>appears in: ${characterData.episode.length} episodes </h3>
-</div>
+const skeletonComponent = () => `
+  <h1>Rick & Morty app</h1>
+  <div class="characters"></div>
+  <div class="buttons"></div>
+`
+
+const characterComponent = (characterData) => ` 
+  <div class="char">
+    <img src=${characterData.image}>
+    <h2>${characterData.name}</h2>
+    <h3>appears in: ${characterData.episode.length} episodes</h3>
+  </div>
 `
 
 const buttonComponent = (id, text) => `<button id=${id}>${text}</button>`
 
-const makeDomFromDta = (data,rootElement) => {
-    const info = data.info
-    const characters = data.results
-
-    characters.forEach(character =>
-        rootElement.insertAdjacentHTML(
-          "beforeend",
-          characterComponent(character)))
-
-console.log(info)
-
-if(info.prev){
-    rootElement.insertAdjacentHTML("beforeend", buttonComponent("prev", "previous"))
-}
-if(info.next) {
-    rootElement.insertAdjacentHTML("beforeend", buttonComponent("next","next"))
-}
+const buttonEventComponent = (id, url) => {
+  const buttonElement = document.querySelector(`#${id}`)
+  buttonElement.addEventListener("click", () => {
+    console.log(`fetch: ${url}`)
+    rootElement.innerHTML = "LOADING..."
+    fetchUrl(url).then(data => makeDomFromData(data, rootElement))
+  })
 }
 
+const makeDomFromData = (data, rootElement) => {
+  rootElement.innerHTML = skeletonComponent()
 
+  const charactersElement = document.querySelector(".characters")
+  const buttonsElement = document.querySelector(".buttons") 
+
+  const info = data.info
+  const characters = data.results
+
+  characters.forEach(character => charactersElement.insertAdjacentHTML("beforeend", characterComponent(character)))
+
+  if (info.prev) {
+    buttonsElement.insertAdjacentHTML("beforeend", buttonComponent("prev", `
+      <span class="material-icons">arrow_back</span>
+    `))
+    buttonEventComponent("prev", info.prev)
+  }
+
+  if (info.next) {
+    buttonsElement.insertAdjacentHTML("beforeend", buttonComponent("next", `
+      <span class="material-icons">arrow_forward</span>
+    `))
+    buttonEventComponent("next", info.next)
+  }
+}
 
 const init = () => {
-  fetchUrl("https://rickandmortyapi.com/api/character").then((data) => {
-    console.log(data);
+  rootElement.innerHTML = "LOADING..."
+  fetchUrl("https://rickandmortyapi.com/api/character").then(data => makeDomFromData(data, rootElement))
+}
 
-    const characters = data.results; 
-    const info = data.info; 
-    console.log(characters[0]);
-
-    characters.forEach((character) => {
-      rootElement.insertAdjacentHTML(
-        "beforeend",
-        characterComponent(character)
-      );
-    });
-  });
-};
-
-init();
+init()
